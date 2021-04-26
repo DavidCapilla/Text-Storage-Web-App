02c2baee26096f67d161289bd4c028fb6956d1c4
David Capilla <dcapillag@gmail.com>
2021-04-26 23:50:21 +0200

    dcg | Add Maven project created by spring initialzr

---
+++ src/main/java/com/reflexiones/TextStorageWebApp/TextStorageWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.reflexiones.TextStorageWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextStorageWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextStorageWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reflexiones/TextStorageWebApp/TextStorageWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reflexiones.TextStorageWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextStorageWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
