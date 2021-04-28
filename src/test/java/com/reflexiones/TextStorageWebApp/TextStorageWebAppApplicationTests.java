b9d12ba559682c35541b11cd4896f25ce98f28f3
David Capilla <dcapillag@gmail.com>
2021-04-28 23:49:04 +0200

    dcg | Add index.html file

--- src/test/java/com/reflexiones/TextStorageWebApp/TextStorageWebAppApplicationTests.java
+++ src/test/java/com/reflexiones/TextStorageWebApp/TextStorageWebAppApplicationTests.java
@@ -1,13 +1,24 @@
 package com.reflexiones.TextStorageWebApp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest
 class TextStorageWebAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void homePage() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andExpect(content().string(containsString("Welcome to Reflexiones")));
+    }
 
 }
