4118c91ab685003fa8fb335a4070f56da53e752b
David Capilla <dcapillag@gmail.com>
2021-09-01 13:14:39 +0200

    dcg | 001 | Make User implement UserDetails interface

--- src/main/java/com/reflexiones/textstoragewebapp/user/User.java
+++ src/main/java/com/reflexiones/textstoragewebapp/user/User.java
@@ -1,10 +1,44 @@
 package com.reflexiones.textstoragewebapp.user;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
-public class User {
+public class User implements UserDetails {
 
     private String userName;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
