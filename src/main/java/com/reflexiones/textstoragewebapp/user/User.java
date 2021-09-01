a8d13247e4f0e820e05586a4757c5b2cf7e11f8b
David Capilla <dcapillag@gmail.com>
2021-09-01 13:28:53 +0200

    dcg | 001 | Implement UserDetails interface methods

--- src/main/java/com/reflexiones/textstoragewebapp/user/User.java
+++ src/main/java/com/reflexiones/textstoragewebapp/user/User.java
@@ -2,25 +2,27 @@ package com.reflexiones.textstoragewebapp.user;
 
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Data
 public class User implements UserDetails {
 
-    private String userName;
+    private Long id;
+    private String username;
     private String password;
     private UserRole userRole;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(userRole.name());
+        return Collections.singletonList(authority);
     }
 
     @Override
@@ -30,16 +32,16 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 }
