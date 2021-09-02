dd2f6dd5e2c2f1c08a08bce6bfbeb7e6a26709e8
David Capilla <dcapillag@gmail.com>
2021-09-02 17:45:54 +0200

    dcg | 001 | Add user annotations

--- src/main/java/com/reflexiones/textstoragewebapp/user/User.java
+++ src/main/java/com/reflexiones/textstoragewebapp/user/User.java
@@ -1,19 +1,28 @@
 package com.reflexiones.textstoragewebapp.user;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
 
 @Data
+@NoArgsConstructor
+@Entity
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
     private Long id;
     private String username;
     private String password;
+    @Enumerated(EnumType.STRING)
     private UserRole userRole;
     private Boolean locked = false;
     private Boolean enabled = false;
